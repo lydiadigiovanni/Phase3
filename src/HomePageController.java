b4053ea1fad0d48dcd3e77b35ff0dab888798271
TimCharest <timcharest@verizon.net>
2022-04-08 13:56:38 -0400

    Code for the Homepage

---
+++ src/HomePageController.java
@@ -0,0 +1,47 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomePageController {
+
+    @FXML
+    private Button Island1Button;
+
+    @FXML
+    private Button Island2Button;
+
+    @FXML
+    private Button Island3Button;
+
+    @FXML
+    private Button LogOutButton;
+
+    @FXML
+    private Button ProfileButton;
+
+    @FXML
+    void Island2ButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void island1ButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void island3ButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOutButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void profileButtonPressed(ActionEvent event) {
+
+    }
+
+}
