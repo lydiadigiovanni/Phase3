38b900fefec67bca8900b9cf5c9e6b6104debf3c
TimCharest <timcharest@verizon.net>
2022-04-08 18:00:05 -0400

    reward screen

---
+++ src/UI/Reward/RewardController.java
@@ -0,0 +1,25 @@
+package UI.Reward;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class RewardController {
+
+    @FXML
+    private Button claimPrize;
+
+    @FXML
+    private Label gradeNum;
+
+    @FXML
+    private ImageView prizePhoto;
+
+    @FXML
+    void prizeClaim(ActionEvent event) {
+
+    }
+
+}
