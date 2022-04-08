984352acffc940d3869205609d64015f76886fa7
TimCharest <timcharest@verizon.net>
2022-04-08 14:16:44 -0400

    Homepage

--- src/UI/Homepage/HomePageController.java
+++ src/UI/Homepage/HomePageController.java
@@ -1,3 +1,5 @@
+package UI.Homepage;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
--- src/UI/Homepage/HomePageMain.java
+++ src/UI/Homepage/HomePageMain.java
@@ -1,3 +1,5 @@
+package UI.Homepage;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
