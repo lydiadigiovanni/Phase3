e0471fceed179cd89d135ce134e57f4d2696293a
TimCharest <timcharest@verizon.net>
2022-04-19 14:50:47 -0400

    user input prac2

--- src/Backend/Assessment/KiPractice2.java
+++ src/Backend/Assessment/KiPractice2.java
@@ -40,8 +40,27 @@ public class KiPractice2 extends Assessment{
 
     @Override
     public String[][] generateUserInputQuestion() {
-        // TODO Auto-generated method stub
-        return null;
+        int random = ThreadLocalRandom.current().nextInt(2);
+        String userInput[][] = new String[2][1];
+        switch (random) {
+            case 0:
+                userInput[0] = new String[]{"0"};
+                userInput[1] = generateQuestionOne();
+                break;
+            case 1: 
+                userInput[0] = new String[]{"1"};
+                userInput[1] = generateQuestionTwo();
+                break;
+            case 2:
+                userInput[0] = new String[]{"2"};
+                userInput[1] = generateQuestionThree();
+                break;
+            case 3:
+                userInput[0] = new String[]{"3"};
+                userInput[1] = generateQuestionFour();
+                break;
+        }
+        return userInput;
     }
     
     //Question One: What comes after numbers "randomly generated sequence"?
@@ -56,7 +75,7 @@ public class KiPractice2 extends Assessment{
         randomNumber = rng + 6; //Answer is always +6 because thats 2 numbers higher than list[2]
         String[] question = new String[6]; //Array will hold question and answers
         //0th index: actual question
-        question[0] = "What comes after numbers " + list[0] + ", " + list[1] + ", " + list[2]; //list[0], list[1], list[2] = the numbers in the sequence
+        question[0] = list[0] + ", " + list[1] + ", " + list[2] + ", ____"; //list[0], list[1], list[2] = the numbers in the sequence
         question[1] = Integer.toString(randomNumber); //Answ:er
         question[2] = Integer.toString(randomNumber-1); //Wrong answer
         question[3] = Integer.toString(randomNumber+1); //Wrong answer
@@ -70,7 +89,7 @@ public class KiPractice2 extends Assessment{
         String[] question = new String[6]; //Array will hold questions and answers
         /*0th index: actual question; we add 3 to the first number to get the second number; 
         and we add 6 to the second number to get the 3rd number*/
-        question[0] = "What comes after numbers " + randomNumber + ", " + (randomNumber + 3) + ", " + (randomNumber + 6);
+        question[0] = randomNumber + ", " + (randomNumber + 3) + ", " + (randomNumber + 6) + ", ____";
         //1st index: correct answer; we add 9 to the first number to get the next number in the sequence
         question[1] = Integer.toString(randomNumber + 9);
         question[2] = Integer.toString(randomNumber-1); //Wrong answer
@@ -85,11 +104,11 @@ public class KiPractice2 extends Assessment{
         String[] question = new String[6]; //Array will hold question and answers
         /*0th index: actual question; they will be filling in the second number which is
         why we add 6 rather than adding 3 to randomNumber*/
-        question[0] = "What is the missing number " + randomNumber + ", " + "____" + ", " + (randomNumber + 6) + ", " + (randomNumber + 9);
+        question[0] = randomNumber + ", " + "____" + ", " + (randomNumber + 6) + ", " + (randomNumber + 9);
         question[1] = Integer.toString(randomNumber + 3); //Second number in sequence is answer; so we add 3!
         question[2] = Integer.toString(randomNumber-1); //Wrong answer
         question[3] = Integer.toString(randomNumber+1); //Wrong answer
-        question[4] = Integer.toString(randomNumber+3); //Wrong answer
+        question[4] = Integer.toString(randomNumber+4); //Wrong answer
         return question; 
     }
 
@@ -99,7 +118,7 @@ public class KiPractice2 extends Assessment{
         String[] question = new String[6]; //Array will hold question and answers
         /*0th index: actual question; they will be filling in the third number which is
         why we add 6 to randomNumber rather than 4 after the blank*/
-        question[0] = "What is the missing number " + randomNumber + ", " + (randomNumber + 2) + ", " + "____" + ", " + (randomNumber + 6);
+        question[0] =  randomNumber + ", " + (randomNumber + 2) + ", " + "____" + ", " + (randomNumber + 6);
         question[1] = Integer.toString(randomNumber + 4); //Correct answer is 4 greater than randomNumber
         question[2] = Integer.toString(randomNumber-1); //Wrong answer
         question[3] = Integer.toString(randomNumber+1); //Wrong answer
