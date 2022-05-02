ce7136a87be7cdec61a9e896e44e68f631c5f1e4
lydiadigiovanni <99436931+lydiadigiovanni@users.noreply.github.com>
2022-05-02 17:48:54 -0400

    A lot
    generators, login UI, create account UI

---
+++ src/UI/Login/CreateAccountMain2.java
@@ -0,0 +1,32 @@
+package UI.Login;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+ 
+public class CreateAccountMain2 extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+  try {
+    //Retrieve the fxml for the login screen
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CreateAccountView2.fxml"));
+    Parent root = (Parent) fxmlLoader.load(); //Assign that fxml as the parent node
+    Scene scene = new Scene(root); //Create a new scene containing root
+     primaryStage.setTitle("Math Islands"); //Title will always be the name of the software
+     primaryStage.setScene(scene); //Ensure the scene is set as the primaryStage
+     primaryStage.show(); //Make visible
+  }
+  
+  catch (IOException e) {
+     e.printStackTrace();
+
+  }
+}
+ public static void main(String[] args) {
+        launch(args);
+    }
+}
