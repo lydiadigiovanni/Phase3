d2e4194f6319ab206ddcc14348d9988cb4c8ca59
greenwolf5 <25jazz25@gmail.com>
2022-05-10 00:51:54 -0400

    Update TFPractice2.java
    test tim

--- src/Backend/Assessment/TFPractice2.java
+++ src/Backend/Assessment/TFPractice2.java
@@ -74,6 +74,8 @@ public class TFPractice2 extends Assessment {
         System.out.println(question3[2]);
         System.out.println(question3[3]);
         System.out.println(question3[4]);
+
+        System.out.println("test " + null);
     }
 
     // Question One: Which number is a factor of " "?
@@ -129,7 +131,18 @@ public class TFPractice2 extends Assessment {
         int rng3 = random.nextInt(9); // random number 3 for ones place
 
         String[] questionThree = new String[6]; // Array will hold question & possible answers
-        questionThree[0] = "Round " + rng + rng2 + rng3 + " to the 100th place";
+        if(rng == 0) {
+            if (rng2 == 0) {
+                questionThree[0] = "Round " + rng3 + "to the 100th place";
+                
+            }
+            else {
+                questionThree[0] = "Round " + rng2 + rng3 + "to the 100th place";
+            }
+        }
+        else {
+            questionThree[0] = "Round " + rng + rng2 + rng3 + "to the 100th place";
+        }
 
         if (rng2 < 5) {
 
@@ -142,6 +155,18 @@ public class TFPractice2 extends Assessment {
             rng2 = 0;
             rng3 = 0;
 
+        }
+        if(rng == 0) {
+            if (rng2 == 0) {
+                questionThree[1] = "" + rng3;
+                
+            }
+            else {
+                questionThree[1] = "" + rng2 + rng3;
+            }
+        }
+        else {
+            questionThree[0] = "" + rng + rng2 + rng3;
         }
 
         questionThree[1] = rng + "" + rng2 + "" + rng3; // Actual answer
